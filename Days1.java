9eecbe71355d5cab2f729dcd28334e3b34260fee
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-08 10:59:55 +0800

    Add files via upload

---
+++ Days1.java
@@ -0,0 +1,7 @@
+package TiaraRamadhani;//package
+
+public class Days1 {// class
+    public static void main(String[] args) { //class main
+        System.out.println("Hello World"); //tampilkan hello world
+    }
+}
