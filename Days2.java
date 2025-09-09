d706d92e9a7dac8b7201efa79bd30d4da4356e83
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-09 16:48:51 +0800

    Add files via upload

---
+++ Days2.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days2 {
+    public static void main(String[] args) {
+        System.out.print("Hello World");// cetak teks tanpa baris baru
+        System.out.println("Hello World");// cetak teks tanpa baris baru
+        int angka = 18;
+        System.out.printf("%d" , angka );// cetak dengan format yang rapi
+    
+}    
+}
