e8da66da4d681caa561d45bf35d959a49ad31eb6
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-10 17:29:06 +0800

    Add files via upload

---
+++ Days3.java
@@ -0,0 +1,26 @@
+//jenis_jenis komentar
+// "//", "/*....*/", "/**....*/"
+
+package TiaraRamadhani;
+
+public class Days3 {
+
+    /**
+     * komentar yang diawali garis miring 1 dan bintang 2
+     * diakhiri dengan 1 bintang dan 1 garis miring
+     * dan setiap barisnya ada 1 bintang
+     * biasanya dipakai di atas class, method atau variable
+     * komentar ini berfungsi untuk membuat dokumentasi program (JavaDoc)
+     */
+    
+    public static void main(String[] args) {
+        //garis miring 2 komentar hanya satu baris
+        System.out.println("Hello Word");//tambahkan 2 garis miring di akhir kode untuk komentar
+
+        /*
+        tambahkan garis miring 1 dan tanda bintang 1
+        dan di akhiri bintang 1 dan garis miring 1
+        untuk komentar yang banyak bari
+         */
+    }
+}
