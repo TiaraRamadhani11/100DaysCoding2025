46448211ac147895138e46fcbfc7f4cd65aa613d
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-11 19:28:58 +0800

    Add files via upload

---
+++ Days4.java
@@ -0,0 +1,20 @@
+package TiaraRamadhani;
+
+public class Days4 {
+    public static void main(String[] args) {
+        System.out.println("Hello \n World");//tanda \n untuk pindahkan teks setelahnya jadi baris baru
+
+        System.out.println("Nama :\tTiara"); //tanda \t untuk memberikan spasi lebar seperti Tab di keyboard
+        System.out.println("Kelas :\tXII RPL");
+
+        System.out.println("Saya : \"Ayo kita belajar.\"");// tanda \" untuk menampilkan tanda kutip di console
+
+        System.out.println("\'T\'");// \' untuk menampilkan tanda kutip 1 di console
+
+        System.out.println("Folder : C:\\User\\Tiara"); // untuk menampilkan backslash \ karna kalau 1 tidak bisa
+
+        System.out.println("Hell\bo");// \b untuk menghapus huruf sebelum tanda \b
+
+
+    }
+}
