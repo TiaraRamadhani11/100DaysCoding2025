d64a656723f015ac09c83ae78929cdfd1aaf0de5
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-12 18:48:52 +0800

    Add files via upload

---
+++ Days5.java
@@ -0,0 +1,15 @@
+package TiaraRamadhani;
+
+public class Days5 {
+    public static void main(String[] args) {
+        byte angka_Byte = 34; // byte range = -128 sd 127
+        short angka_short = 69;// short range = -32.768 sd 32.767
+        int angka_int = 585; // int range = -2^31 sd 2^31-1
+        long angka_long = 472346; // long range = -2^63 sd 2^63-1
+
+        System.out.println(angka_Byte);
+        System.out.println(angka_short);
+        System.out.println(angka_int);
+        System.out.println(angka_long);
+    }
+}
