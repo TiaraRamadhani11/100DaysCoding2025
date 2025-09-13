553329fc154ae2e406dd0f567c80da3663538e77
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-13 17:13:43 +0800

    Add files via upload

---
+++ Days6.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days6 {
+    public static void main(String[] args) {
+        double Angka_double = 383.455; //tipe data double di gunakan untuk angka desimal
+        float Angka_float = 473.46f; //tipe data float sama seperti double tapi kalau float di tambahkan huruf f di akhir angka
+
+        System.out.println("double : " + Angka_double);
+        System.out.println("float : " + Angka_float);
+    }
+}
