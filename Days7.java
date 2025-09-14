c9dfe012c2a6f388d6c28ac77340a3cbb55dd4b8
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-14 19:17:38 +0800

    Add files via upload

---
+++ Days7.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days7 {
+    public static void main(String[] args) {
+        char karakter = 'A'; // menyimpan 1 karakter seperti huruf, angka atau simbol
+        boolean TF = true; // hanya memiliki dua nilai true dan false
+
+        System.out.println(karakter);
+        System.out.println(TF);
+    }
+}
