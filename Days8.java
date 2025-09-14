5ae1bb71055719e2b604ce1e5e442d52848ddbb1
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-14 19:36:16 +0800

    Add files via upload

---
+++ Days8.java
@@ -0,0 +1,10 @@
+package TiaraRamadhani;
+
+public class Days8 {
+    public static void main(String[] args) {
+        //tipe data yang dapat menyimpan serangkaian teks bisa terdiri dari huruf, angka atau simbol
+        //di apit sama dua tanda kutip
+        String teks = "Hai semuanya! saya tr, umur saya 18";
+        System.out.println(teks);
+    }
+}
