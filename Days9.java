b22db6d53facf90ac0859247c544fe1a231bf61a
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-16 21:05:29 +0800

    Add files via upload

---
+++ Days9.java
@@ -0,0 +1,12 @@
+package TiaraRamadhani;
+
+public class Days9 {
+    public static void main(String[] args) {
+        int angka;//deklarasi, membuat variable nya dulu
+        angka = 34;// isi nilai
+        angka = 56; // update angka
+        System.out.println(angka);// angka yang tampil adalah yang kedua setelah di update yaitu 56
+
+        int nilai = 111006; // langsung mengisi nilai nya
+    }
+}
