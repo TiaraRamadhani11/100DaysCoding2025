4a0fdd505bd98caf3cba5b6387de043d146d1b5c
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-17 21:23:58 +0800

    Add files via upload

---
+++ Days11.java
@@ -0,0 +1,48 @@
+package TiaraRamadhani;
+
+import java.util.Scanner;//import kelas scanner
+
+public class Days11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);// membuat objek scanner
+
+        System.out.print("Nama : ");
+        String nama = input.nextLine();
+
+        System.out.print("umur : ");
+        int umur = input.nextInt();
+
+        System.out.print("dua digit terakhir NIM : ");
+        byte nim = input.nextByte();
+
+        System.out.print("no sepatu : ");
+        short noSepatu = input.nextShort();
+
+        System.out.print("jarak rumah ke kampus : ");
+        long jarak = input.nextLong();
+
+        System.out.print("Berat Badan : ");
+        double bb = input.nextDouble();
+
+        System.out.print("Tinggi Badan : ");
+        float tb = input.nextFloat();
+
+        System.out.print("kelas : ");
+        char kls = input.next().charAt(0);
+
+        System.out.print("saya keren? ");
+        boolean b = input.nextBoolean();
+
+
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+        System.out.println("Dua digit terakhir NIM : " + nim);
+        System.out.println("No Sepatu : " + noSepatu);
+        System.out.println("Jarak rumah ke kampus : " + jarak + "KM");
+        System.out.println("Berat Badan : " + bb);
+        System.out.println("Tinggi Badan : " + tb);
+        System.out.println("Kelas : " + kls);
+        System.out.println("Saya Keren? " + b);
+
+    }
+}
