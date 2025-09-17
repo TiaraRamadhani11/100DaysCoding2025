4985dc4935c3382f75eb50a17c80e5ab0468a8db
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-17 21:13:36 +0800

    Add files via upload

---
+++ Days10.java
@@ -0,0 +1,9 @@
+package TiaraRamadhani;
+
+public class Days10 {
+    public static void main(String[] args) {
+        final int angka;
+        angka = 43;// nilai dalam suatu variable tetap, tidak bisa di update
+        System.out.println(angka);
+    }
+}
