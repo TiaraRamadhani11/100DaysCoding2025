ea8efcfd471b8f4945b6d7728ad1540dac83d235
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-22 00:18:58 +0800

    Add files via upload

---
+++ Days15.java
@@ -0,0 +1,15 @@
+package TiaraRamadhani;
+
+public class Days15 {
+    public static void main(String[] args) {
+        int angka1 = 11;
+        int angka2 = 10;
+        int jumlah = angka1 + angka2;//operator aritmatika penjumlahan
+        System.out.println(jumlah);//output 21
+
+        double angka3 = 26;
+        double angka4 = 1;
+        double kurang = angka3 - angka4; //operator aritmatika pengurangan
+        System.out.println(kurang);//output 25.0
+    }
+}
