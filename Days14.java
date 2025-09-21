4f5b1d6c2ca54181781a9f1e2207fa2a4821a649
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-21 19:59:33 +0800

    Add files via upload

---
+++ Days14.java
@@ -0,0 +1,13 @@
+package TiaraRamadhani;
+
+public class Days14 {
+    public static void main(String[] args) {
+        String angka1 = "11";
+        String angka2 = "10";
+
+        double angka3 = Double.parseDouble(angka1);//mengubah string ke double
+        int angka4 = Integer.parseInt(angka2);//mengubah string ke int
+
+        System.out.println(angka3 + angka4); //outputnya = 21.0
+    }
+}
