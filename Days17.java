4fb7d55ece74baac758773dd590950a41b2628ec
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-24 20:26:58 +0800

    Add files via upload

---
+++ Days17.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days17 {
+    public static void main(String[] args) {
+        //operator modulo
+        int angka1 = 11;
+        int angka2 = 10;
+
+        System.out.println(angka1 % angka2);// outputnya 1
+    }
+}
