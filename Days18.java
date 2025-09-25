2fe4eb21c50345b5ead19766bd2b1451fd943d36
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-25 22:41:43 +0800

    Add files via upload

---
+++ Days18.java
@@ -0,0 +1,14 @@
+package TiaraRamadhani;
+
+public class Days18 {
+    public static void main(String[] args) {
+        byte angkabyte = 10;
+        int angkaint = angkabyte;//byte ke int
+        long angkalong = angkaint;//int ke long
+        double angkadouble = angkalong;//long ke double
+
+        System.out.println("byte ke int : " + angkaint);
+        System.out.println("int ke long : " + angkalong);
+        System.out.println("long ke double : " + angkadouble);
+    }
+}
