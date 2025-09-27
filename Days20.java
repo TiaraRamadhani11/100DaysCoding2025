f68af9bb070cc3c802fd83d96a6076d365fbece6
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-27 23:10:30 +0800

    Days20.java

---
+++ Days20.java
@@ -0,0 +1,17 @@
+package TiaraRamadhani;
+
+public class Days20 {
+    public static void main(String[] args) {
+        int angka1 = 11;
+        float angka2= 10;
+        boolean b = true;
+
+        String s1 = String.valueOf(angka1);
+        String s2 = String.valueOf(angka2);
+        String s3 = String.valueOf(b);
+
+        System.out.println("int ke String: " + s1);
+        System.out.println("double ke String: " + s2);
+        System.out.println("boolean ke String: " + s3);
+    }
+}
