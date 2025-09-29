31d78c62a3459ef294c8649a25d8e16a8adac076
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-29 21:40:54 +0800

    Days22.java

---
+++ Days22.java
@@ -0,0 +1,12 @@
+package TiaraRamadhani;
+
+public class Days22 {
+    public static void main(String[] args) {
+        double sisi = 11;
+        // Hitung luas
+        double luas = sisi * sisi;
+
+        // Tampilkan hasil
+        System.out.println("Luas persegi :" + luas);
+    }
+}
