740cc9015cf261855dad8747c0433fd928db23d4
TiaraRamadhani11 <tr987776@gmail.com>
2025-09-30 22:39:04 +0800

    Days23.java

---
+++ Days23.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days23 {
+    public static void main(String[] args) {
+        double panjang =11;
+        double lebar =10;
+
+        double luas = panjang * lebar;
+        System.out.println ("Luas Persegi Panjang : " + luas);
+    }
+}
