4343513c5a5ef559135c2cf7d12bb8b993284559
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-01 19:54:38 +0800

    Days24.java

---
+++ Days24.java
@@ -0,0 +1,10 @@
+package TiaraRamadhani;
+
+public class Days1 {
+    public static void main(String[] args) { 
+      double r = 11;
+      double phi = 3.14;
+      //rumus luas lingkaran= 
+      
+    }
+}
