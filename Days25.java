1d20a11498ac9d319d3ff22d2e4d5deb12b6bffb
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-02 21:00:31 +0800

    Days25.java

---
+++ Days25.java
@@ -0,0 +1,14 @@
+package TiaraRamadhani;
+
+public class Days25 {
+    public static void main(String[] args) {
+        System.out.println("Perulangan Increment:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Perulangan Decrement:");
+        for (int j = 5; j >= 1; j--) {
+            System.out.println("j = " + j);
+        }
+    }
+}
