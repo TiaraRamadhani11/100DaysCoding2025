8579911c495f3c4869fe9d73745103969b3b711e
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-03 21:56:28 +0800

    Days26.java

---
+++ Days26.java
@@ -0,0 +1,27 @@
+package TiaraRamadhani;
+
+public class Days26 {
+    public static void main(String[] args) {
+        int angka = 26;
+
+        // +=
+        angka += 1; 
+        System.out.println("26 += 1 : " + angka);
+
+        // -=
+        angka -= 11;
+        System.out.println("27 -= 11 : " + angka);
+
+        // *=
+        angka *= 10;
+        System.out.println("16 *= 10 : " + angka);
+
+        // /=
+        angka /= 20;
+        System.out.println("160 /= 20 : " + angka);
+
+        // %=
+        angka %= 6;
+        System.out.println("8 %= 6 : " + angka);
+    }
+}
