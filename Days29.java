aff0d4fff6b30c4cb1514f927130d892be64af9a
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-06 20:57:54 +0800

    Days29.java

---
+++ Days29.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days28 {
+    public static void main(String[] args) {
+        //operator perbandingan < dan >
+        int angka1 = 11;
+        int angka2 = 10;
+        System.out.println("apakah angka1 lebih kecil dari angka2 ? " + (angka1 < angka2));//output false
+        System.out.println("apakah angka1 lebih besar dari angka2 ? " + (angka1 > angka2);//output true
+    }
+}
