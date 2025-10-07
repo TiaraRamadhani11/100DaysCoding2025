3b85c0fbd0cba1efea9bbb814fc1e7ef19d20485
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-07 21:58:34 +0800

    Add files via upload

---
+++ Days30.java
@@ -0,0 +1,16 @@
+package TiaraRamadhani;
+
+public class Days30 {
+    public static void main(String[] args) {
+        //operator perbandingan <= (lebih kecil sama dengan) dan >= (lebih besar sama dengan)
+
+        int angka1 = 11;
+        int angka2 = 10;
+        int angka3 = 11;
+
+        System.out.println(angka1 <= angka2);//false
+        System.out.println(angka1 >= angka2);//true
+        System.out.println(angka1 <= angka3);//true
+
+    }
+}
