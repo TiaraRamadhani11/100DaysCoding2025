42c07eac3677075a2fc87d32e17811d141c99f91
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-08 22:04:59 +0800

    Days31.java

---
+++ Days31.java
@@ -0,0 +1,14 @@
+package TiaraRamadhani;
+
+public class Days31 {
+    public static void main(String[] args) {
+        //operator logika &&
+
+        int angka1 = 11;
+        int angka2 = 10;
+
+        System.out.println((angka1 < angka 2)&&(angka1 > angka 2));//false
+        System.out.println((angka1 >= 2)&&(angka2 <= 20));//true
+
+    }
+}
