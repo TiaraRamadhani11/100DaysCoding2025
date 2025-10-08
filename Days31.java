eef2124e831326e94b648510d1b4602f571de6b3
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-08 22:22:00 +0800

    Add files via upload

---
+++ Days31.java
@@ -0,0 +1,14 @@
+package TiaraRamadhani;
+
+public class Days31 {
+    public static void main(String[] args) {
+        //operator logika &&
+
+        int angka1 = 11;
+        int angka2 = 10;
+
+        System.out.println((angka1 < angka2)&&(angka1 > angka2));//false
+        System.out.println((angka1 >= 2)&&(angka2 <= 20));//true
+
+    }
+}
