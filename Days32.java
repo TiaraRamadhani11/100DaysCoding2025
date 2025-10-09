ad493adcd4f47b18701af6618e41a41b00d6fff2
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-09 22:34:28 +0800

    Days32.java

---
+++ Days32.java
@@ -0,0 +1,10 @@
+public class Days32 {
+    public static void main(String[] args) {
+        int angka1 = 11;
+        int angka2 = 10;
+
+        System.out.println(angka1 > 2 || angka2 > 2);  // true
+        System.out.println(angka1 > 20 || angka2 > 2);  // true
+        System.out.println(angka1 < 1 || angka2 < 1);  // false
+    }      
+}
