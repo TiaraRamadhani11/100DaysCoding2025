f7231c725df9e66714631517486c283a6a629fe9
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-10 21:29:15 +0800

    Days33.java

--- Days33.java
+++ Days33.java
@@ -2,7 +2,7 @@ package TiaraRamadhani;
 
 public class Days33 {
     public static void main(String[] args) {
-        //operator logika not !=
+        //operator logika not !
         boolean kondisi = false;
         System.out.println(kondisi);  // false
         System.out.println(!kondisi);  // true
