10478ce4f17f35f2c42c07152afee0110087be2e
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-13 22:40:16 +0800

    Add files via upload

---
+++ Days36.java
@@ -0,0 +1,23 @@
+package TiaraRamadhani;
+
+import java.util.Scanner;
+
+public class Days36 {
+    public static void main(String[] args) {
+        Scanner tr = new Scanner(System.in);
+        System.out.print("Masukkan umur : ");
+        int umur = tr.nextInt();
+        System.out.print("Masukkan Suhu Tubuh : ");
+        int suhu = tr.nextInt();
+
+        if (umur >= 18){
+            if (suhu <= 37){
+                System.out.println("Anda Boleh Masuk");
+            }else {
+                System.out.println("Dilarang masuk! suhu tubuh anda tunggi");
+            }
+        }else{
+            System.out.println("Umur tidak mencukupi");
+        }
+    }
+}
