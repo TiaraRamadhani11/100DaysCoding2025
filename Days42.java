62d56529889cd130fb07c3e17f3c3b8819f2b664
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-19 23:04:26 +0800

    Days42.java

--- Days42.java
+++ Days42.java
@@ -10,14 +10,14 @@ public class Days42 {
 
         System.out.println("\nKelipatan 3:");
         for (int i = 1; i <= batas; i++) {
-            if (i % 3 == 0 && i % 5 != 0) {
+            if (i % 3 == 0) {
                 System.out.print(i + " ");
             }
         }
 
         System.out.println("\nKelipatan 5:");
         for (int i = 1; i <= batas; i++) {
-            if (i % 5 == 0 && i % 3 != 0) {
+            if (i % 5 == 0) {
                 System.out.print(i + " ");
             }
         }
