cb89d4788af94913446e27251282ddc602d39157
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-30 23:26:59 +0800

    Days53.java

---
+++ Days53.java
@@ -0,0 +1,13 @@
+package TiaraRamadhani;
+
+public class Days53 {
+    public static void main(String[] args) {
+        for(int i = 1; i < 11; i++) {
+            if(i == 5) {
+                break; 
+            }
+            System.out.println(i);
+        }
+
+    }
+}
