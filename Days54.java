d955f1b41da39d9adfacf255078b3983ad6c6c4d
TiaraRamadhani11 <tr987776@gmail.com>
2025-10-31 23:24:38 +0800

    Add files via upload

---
+++ Days54.java
@@ -0,0 +1,13 @@
+package TiaraRamadhani;
+
+public class Days54 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue; // lewati angka 3
+            }
+            System.out.println(i);
+        }
+
+    }
+}
