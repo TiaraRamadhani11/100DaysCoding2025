9f193624a7e2c3af2caa735deeaf8ad245722c95
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-01 23:30:53 +0800

    Days55.java

---
+++ Days55.java
@@ -0,0 +1,23 @@
+package TiaraRamadhani;
+
+import java.util.Scanner;
+
+public class Days55 {
+    public static void main(String[] args) {
+        Scanner tr = new Scanner(System.in);
+        
+        System.out.println("Batas bawah : ");
+        int bw = tr.nextInt();
+        System.out.println("Batas atas  : ");
+        int ba = tr.nextInt();
+        System.out.println("Angka : ");
+        int angka = juni.nextInt();
+
+
+        if(angka > bw && angka < ba){
+            System.out.println("YAYAYAYA");
+        }else{
+            System.out.println("NONONONO");
+        }
+    }
+}
