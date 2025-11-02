cad4854cdadfa42b6dc221bab19e2e4beb818931
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-02 23:22:43 +0800

    Add files via upload

---
+++ Days56.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days56 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Angka: " + i);
+            i++;
+        } while (i < 10);
+    }
+}
