8bac6738a25499087580620b72e4b39d7ccbc255
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-19 22:33:23 +0800

    Update Days67.java

--- Days67.java
+++ Days67.java
@@ -1,7 +1,7 @@
 package TiaraRamadhani;
 
 public class Days67{
-  public static void main (String [] args{
+  public static void main (String [] args){
     for(int i = 1; i <= 10; i++){
       System.out.print("-");
     }
