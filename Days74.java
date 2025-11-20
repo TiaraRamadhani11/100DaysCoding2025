fe292f1489cb44913a8304a7499469aa2c2b43c2
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-20 17:40:58 +0800

    Add files via upload

---
+++ Days74.java
@@ -0,0 +1,70 @@
+package TiaraRamadhani;
+
+public class Days74 {
+    public static void main(String[] args) {
+//        String nama = "tr";
+//        int umur = 19;
+//        float tb = 152.5f;
+//        double bb = 44.5;
+//        short angka1 = 1;
+//        byte angka2 = 2;
+//        long tahun = 2025;
+//        char kelas = 'c';
+//        boolean mangEakk = true;
+//
+//        System.out.println(nama);
+//        System.out.print(tahun);
+//        System.out.printf(" %d",umur);
+
+//        for (int i = 3; i <= 100 ; i+=3) {
+//            if (i >= 50){
+//                break;
+//            }
+//            System.out.println(i);
+//            if (i % 2==0) {
+//                System.out.println(i);
+//            }
+//        }
+
+
+//        Scanner tr =  new Scanner(System.in);
+//        System.out.print("masukkan angka 1-3 : ");
+//        int input = tr.nextInt();
+//
+//        switch (input){
+//            case 1 :
+//                System.out.println("apalah");
+//                break;
+//            case 2 :
+//                System.out.println("hallallah");
+//                break;
+//            case 3 :
+//                System.out.println("bilang dlu anjayy");
+//                break;
+//            default :
+//                System.out.println("nd valid broo!!!");
+//                break;
+//        }
+
+
+//        int nilai = 75;
+//        String status = (nilai >= 70) ? "Lulus" : "Tidak Lulus";
+//
+//        System.out.println(status);
+
+        int i = 1;
+        do {
+            if (i == 3) {
+                i++;
+                continue;
+            }
+            if (i == 7) {
+                break;
+            }
+            System.out.println(i);
+            i++;
+
+        } while (true);
+
+    }
+}
