1d9139586510f551d3b1f4c0c14de0718533c580
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-21 23:15:16 +0800

    Days75.java

---
+++ Days75.java
@@ -0,0 +1,12 @@
+package TiaraRamadhani;
+
+public class Days75 {
+    public static void main(String[] args) {
+        String kalimat= "Bilang Dulu Anjayy";
+
+        System.out.println("Kalimat asli: " + kalimat);
+        System.out.println("Panjang     : " + kalimat.length());
+        System.out.println("Huruf besar : " + kalimat.toUpperCase());
+        System.out.println("Huruf kecil : " + kalimat.toLowerCase());
+    }
+}
