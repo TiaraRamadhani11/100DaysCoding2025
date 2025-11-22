8670d77df7108e08e683b85bd8ff817acdbc3272
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-22 23:25:46 +0800

    Days76.java

---
+++ Days76.java
@@ -0,0 +1,22 @@
+package TiaraRamadhani;
+
+public class Days76 {
+  public static void main(String[] args) {
+
+        String kata1 = "Apalah";
+        String kata2 = "apalah";
+        String kata3 = "";
+
+        // equals()
+        System.out.println(kata1.equals(kata2));
+
+        // equalsIgnoreCase()
+        System.out.println(kata1.equalsIgnoreCase(kata2));
+
+        // contains()
+        System.out.println(kata1.contains("apa"));
+
+        // isEmpty()
+        System.out.println("kata3.isEmpty());
+    }
+}
