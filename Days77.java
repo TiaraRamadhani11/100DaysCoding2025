74eb3540365149fab0c47ffa185eaffeba10dca9
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-23 22:58:22 +0800

    Add files via upload

---
+++ Days77.java
@@ -0,0 +1,12 @@
+package TiaraRamadhani;
+
+public class Days77 {
+    public static void main(String[] args) {
+        String kata1 = "Apalah";
+        String kata2 = " oke ";
+
+        System.out.println(kata1.substring(0,3));
+        System.out.println(kata1.replace('a','o'));
+        System.out.println(kata2.trim());
+    }
+}
