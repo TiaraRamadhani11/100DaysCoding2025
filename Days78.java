55eb66820796328fc471b11dc0cf3544dad432cd
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-24 23:11:01 +0800

    Days78.java

---
+++ Days78.java
@@ -0,0 +1,24 @@
+package TiaraRamadhani;
+
+public class Days78 {
+    public static void main(String[] args) {
+        String teks = "apalah"
+
+        System.out.println("\n--- HASIL ---");
+      
+        // 1. length()
+        System.out.println("Panjang teks: " + teks.length());
+
+        // 2. toUpperCase()
+        System.out.println("Teks UPPERCASE: " + teks.toUpperCase());
+
+        // 3. toLowerCase()
+        System.out.println("Teks lowercase: " + teks.toLowerCase());
+
+        // 4. contains()
+        System.out.println("Apakah mengandung kata 'java'? " + teks.contains("saya"));
+
+        // 5. isEmpty()
+        System.out.println("Apakah teks kosong? " + teks.isEmpty());
+    }
+}
