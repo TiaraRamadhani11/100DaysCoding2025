6f60b08cd402399d75399bf1a19028787af2a5c2
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-27 23:24:09 +0800

    Days81.java

---
+++ Days81.java
@@ -0,0 +1,8 @@
+package TiaraRamadhani;
+
+public class Days81 {
+    public static void main(String[] args) {
+        int [] angka = {11, 10, 26, 1};
+         System.out.println("tampilkan indeks ke 2: " + angka[2]);
+        }
+    }
