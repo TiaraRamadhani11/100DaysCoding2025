b25c558176b869f156be5e9a963eb99145cc7853
TiaraRamadhani11 <tr987776@gmail.com>
2025-11-28 23:15:29 +0800

    Days82.java

---
+++ Days82.java
@@ -0,0 +1,12 @@
+package TiaraRamadhani;
+
+public class Days82 {
+    public static void main(String[] args) {
+      
+        int[] angka = {11, 10, 26, 1};
+
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+    }
+}
