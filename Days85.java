38d907a7c1d18edcf1857f805ae4921cf47af243
TiaraRamadhani11 <tr987776@gmail.com>
2025-12-01 20:54:11 +0800

    Days85.java

---
+++ Days85.java
@@ -0,0 +1,16 @@
+package TiaraRamadhani;
+
+public class Days85{
+    public static void main(String[] args) {
+
+        int[] angka = {11, 10, 26, 1};
+
+        int max = angka[0];
+        for (int i = 1; i < angka.length; i++) {
+            if (angka[i] > max) {
+                max = angka[i];
+            }
+        }
+        System.out.println("Angka maksimal adalah: " + max);
+    }
+}
