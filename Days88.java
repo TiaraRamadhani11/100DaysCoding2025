46cbb11f17ebfb4dcc106ab2a2e670993846a49e
TiaraRamadhani11 <tr987776@gmail.com>
2025-12-03 22:56:02 +0800

    Add files via upload

---
+++ Days88.java
@@ -0,0 +1,12 @@
+package TiaraRamadhani;
+
+public class Days88 {
+    public static void main(String[] args) {
+        int[] array = {11, 10, 26, 1}; 
+
+        System.out.println("Isi array:");
+        for (int elemen : array) {
+            System.out.println(elemen);
+        }
+    }
+}
