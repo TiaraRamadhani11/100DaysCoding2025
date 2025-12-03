f10eb903866444cf0562c6a8fcb310e8cfe742e3
TiaraRamadhani11 <tr987776@gmail.com>
2025-12-03 23:09:11 +0800

    Add files via upload

---
+++ Days91.java
@@ -0,0 +1,14 @@
+package TiaraRamadhani;
+
+public class Days91 {
+    public static void kenalan(){
+        System.out.println("Hallo, saya Yaya!!");
+    }
+    public static void sapaOrang(String Nama){
+        System.out.println("Hai, saya " + Nama );
+    }
+    public static void main(String[] args) {
+     kenalan();
+     sapaOrang("yaya");
+    }
+}
