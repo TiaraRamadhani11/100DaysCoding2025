dfb03631c29afb634e7edc1cfb0e60b4c9e5724b
TiaraRamadhani11 <tr987776@gmail.com>
2025-12-03 23:03:22 +0800

    Add files via upload

---
+++ Days89.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days89 {
+    // Method void
+    public static void tampilPesan() {
+        System.out.println("Halo, Saya Yaya!!!");
+    }
+    public static void main(String[] args) {
+        tampilPesan();
+    }
+}
---
+++ Days90.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days90 {
+    public static int jumlah(int a1, int a2) {
+        return a1 + a2;
+    }
+    public static void main(String[] args) {
+        int hasil = jumlah(11, 26);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
