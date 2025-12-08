ae55d3ae6e68c0b74feed10e6b7d0618671f5a8b
TiaraRamadhani11 <tr987776@gmail.com>
2025-12-08 22:54:20 +0800

    Add files via upload

---
+++ Days92.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days92 {
+    public static int kurang(int a, int b) {
+        return a - b;
+    }
+    public static void main(String[] args) {
+        int hasil = kurang(26,11);
+        System.out.println(hasil);
+    }
+}
