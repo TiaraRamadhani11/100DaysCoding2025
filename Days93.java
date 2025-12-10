156218ee4ea97fc22b8ea85b9d5e19a662f83a46
TiaraRamadhani11 <tr987776@gmail.com>
2025-12-10 21:50:46 +0800

    Add files via upload

---
+++ Days93.java
@@ -0,0 +1,22 @@
+package TiaraRamadhani;
+
+public class Days93 {
+    public static void main(String[] args) {
+        //math ceil
+        System.out.println(Math.ceil(4.1));  // 5.0
+        System.out.println(Math.ceil(4.9));  // 5.0
+        System.out.println(Math.ceil(-4.1)); // -4.0
+
+        //math floor
+        System.out.println(Math.floor(4.9));  // 4.0
+        System.out.println(Math.floor(4.1));  // 4.0
+        System.out.println(Math.floor(-4.1)); // -5.0
+
+        //math round
+        System.out.println(Math.round(4.4));  // 4
+        System.out.println(Math.round(4.6));  // 5
+        System.out.println(Math.round(-4.4)); // -4
+        System.out.println(Math.round(-4.6)); // -5
+
+    }
+}
---
+++ Days94.java
@@ -0,0 +1,11 @@
+package TiaraRamadhani;
+
+public class Days94 {
+    static void kata_kata(){
+        System.out.println("Bilang Dulu Anjayy!!");
+    }
+
+    public static void main(String[] args) {
+        kata_kata();
+    }
+}
