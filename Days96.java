04964ccff0144d30b00433b68b55aeac11748076
TiaraRamadhani11 <tr987776@gmail.com>
2025-12-12 22:18:35 +0800

    Days96.java

--- Days96.java
+++ Days96.java
@@ -2,19 +2,22 @@ package TiaraRamadhani;
 
 import java.util.Scanner;
 
-public class Coding96 {
+public class Days96 {
     // Penjumlahan
     public static int tambah(int a, int b) {
         return a + b;
     }
+
     // Pengurangan
     public static int kurang(int a, int b) {
         return a - b;
     }
+
     // Perkalian
     public static int kali(int a, int b) {
         return a * b;
     }
+
     // Pembagian
     public static int bagi(int a, int b) {
         return a / b;
@@ -22,11 +25,12 @@ public class Coding96 {
 
     public static void main(String[] args) {
         Scanner tr = new Scanner(System.in);
-        System.out.print("Masukkan angka1: ");
-        int angka1 = tr.nextInt;
-        System.out.print("Masukkan angka2: ");
-        int angka2 = tr.nextInt;
 
+        System.out.print("Masukkan angka1: ");
+        int angka1 = tr.nextInt();   
+
+        System.out.print("Masukkan angka2: ");
+        int angka2 = tr.nextInt();   
 
         System.out.println("Penjumlahan = " + tambah(angka1, angka2));
         System.out.println("Pengurangan = " + kurang(angka1, angka2));
